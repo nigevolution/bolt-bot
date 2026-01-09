db3c85139661506634b362792b1eaf60be095a85
ELBS <86670001+nigevolution@users.noreply.github.com>
2026-01-09 13:38:13 -0400

    Update start.js

--- start.js
+++ start.js
@@ -2,25 +2,34 @@ import makeWASocket, { DisconnectReason, useMultiFileAuthState } from "@whiskeys
 import qrcode from "qrcode-terminal"
 
 async function start() {
+  console.log("INICIANDO BOT...")
+
   const { state, saveCreds } = await useMultiFileAuthState("./auth")
 
   const sock = makeWASocket({
     auth: state,
-    printQRInTerminal: true
+    printQRInTerminal: false
   })
 
   sock.ev.on("creds.update", saveCreds)
 
-  sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
+  sock.ev.on("connection.update", ({ connection, qr, lastDisconnect }) => {
+    if (qr) {
+      console.log("QR RECEBIDO:")
+      qrcode.generate(qr, { small: true })
+    }
+
     if (connection === "close") {
       if ((lastDisconnect?.error?.output?.statusCode) !== DisconnectReason.loggedOut) {
+        console.log("Reconectando...")
         start()
       } else {
         console.log("Deslogado do WhatsApp.")
       }
     }
+
     if (connection === "open") {
-      console.log("BOT ONLINE 🚀")
+      console.log("BOT CONECTADO COM SUCESSO 🚀")
     }
   })
 }
